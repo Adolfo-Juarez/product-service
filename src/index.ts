bb5e3e75ba5169244b77698152ad2b067fb330af
GJZ26 <adolfogj26@gmail.com>
2025-05-27 09:51:52 -0600

    fix: Product Jenkins file

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,8 @@ app.get('/', (req, res) => {
 
 sequelize.sync({ alter: true })
     .then(() => {
-        app.listen(3001, () => {
-            console.log('Server is running on port 3001');
+        app.listen(3031, () => {
+            console.log('Server is running on port 3031');
         });
     })
     .catch((error) => {
